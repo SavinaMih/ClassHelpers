5a25f121b6b452455d6ae8039c330fe6b63be03c
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2022-07-19 09:39:58 +0300

    Add project files.

---
+++ ClassHelpers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClassHelpers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
