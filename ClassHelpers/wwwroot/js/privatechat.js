f7ded382423a6f89463f4d6e3849e58d18f9c2eb
B. Belchev <74198382+bdbelchev@users.noreply.github.com>
2022-07-20 17:55:46 +0300

    Added private chat page and functionality

---
+++ ClassHelpers/wwwroot/js/privatechat.js
@@ -0,0 +1,34 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+
+//Disable the send button until connection is established.
+document.getElementById("sendButton").disabled = true;
+
+connection.on("PrivateMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${user} said: ${message}`;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var name = document.getElementById("nameInput").value;
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendPrivateMessage", name, user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.textContent = `You said: ${message}`;
+});
