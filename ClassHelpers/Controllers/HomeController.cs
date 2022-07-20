715ec945d063b08135906fc7a1e8d893a79b40f4
B. Belchev <74198382+bdbelchev@users.noreply.github.com>
2022-07-20 12:26:09 +0300

    Created page for public chatroom

--- ClassHelpers/Controllers/HomeController.cs
+++ ClassHelpers/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace ClassHelpers.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Chatroom()
+        {
+            return View();
+        }
     }
 }
