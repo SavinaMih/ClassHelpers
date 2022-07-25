40f074fcd42f4649393327f50d8d886022f47227
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2022-07-25 13:47:00 +0300

    remove startup

--- ClassHelpers/Program.cs
+++ ClassHelpers/Program.cs
@@ -3,11 +3,6 @@ using ClassHelpers.Hubs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder.Services); // calling ConfigureServices method
-var app = builder.Build();
-startup.Configure(app, builder.Environment); // calling Configure method
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ClassHelpersContextConnection") ?? throw new InvalidOperationException("Connection string 'ClassHelpersContextConnection' not found.");
