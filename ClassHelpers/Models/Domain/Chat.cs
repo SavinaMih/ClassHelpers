0520aa539c21c008458e7f455f6cf289a30354d6
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2022-07-27 14:11:34 +0300

    update models

--- ClassHelpers/Models/Domain/Chat.cs
+++ ClassHelpers/Models/Domain/Chat.cs
@@ -1,6 +1,27 @@
-﻿namespace ClassHelpers.Models.Domain
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ClassHelpers.Models.Domain
 {
     public class Chat
     {
+        [Key]
+        public int ChatId { get; set; }
+
+        [ForeignKey("Sender")]
+        public int SenderId { get; set; }
+        public virtual Account Sender { get; set; }
+
+        [ForeignKey("Receiver")] 
+        public int ReceiverId { get; set; }
+        public virtual Account Receiver { get; set; }
+
+        public virtual List<Message> Messages { get; set; }
+        
     }
 }
