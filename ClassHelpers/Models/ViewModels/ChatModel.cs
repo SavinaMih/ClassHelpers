840dea89bda4ff075332e50dc7e481ebd7d86e5d
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2022-07-27 13:55:36 +0300

    add models

---
+++ ClassHelpers/Models/ViewModels/ChatModel.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ClassHelpers.Models.Domain;
+using ClassHelpers.Models.InputModels;
+
+namespace ClassHelpers.Models.ViewModels
+{
+    public class ChatModel
+    {
+       
+        [Display(Name = "Contact")]
+        public string ContactName { get; set; }
+        public IEnumerable<ChatMessage> MessageList { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter a message before sending it")]
+        [DataType(DataType.Text)]
+        public string MessageInput { get; set; }
+
+        public int ChatId { get; set; }  
+
+        public Message message { get; set; }
+    }
+}
